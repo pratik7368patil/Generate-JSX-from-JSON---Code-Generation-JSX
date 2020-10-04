cb4cc8c3479870923ed80b971877bb0ced5b5fd8
Pratik <pratik@pop-os.localdomain>
2020-10-04 14:27:39 +0530

    new funtion generateJSX added in index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-function generateCodeFromObject(obj){
-    //return a code generated string
-   }
-   
-module.exports=generateCodeFromObject;
 
+function generateJSX(obj) {
+  let res = "<";
+  res += obj.name + " ";
+
+  if (obj.style !== undefined && Object.keys(obj.style).length > 0) {
+    let styleIterator = Object.keys(obj.style);
+    res += "style={{";
+    for (let i = 0; i < styleIterator.length; i++) {
+      res += `${styleIterator[i]} : ${obj.style[styleIterator[i]]}`;
+    }
+    res += "}}";
+  }
+
+  if (obj.children !== undefined && obj.children.length > 0) {
+    res += ">";
+    for (let i = 0; i < obj.children.length; i++) {
+      res += generateJSX(obj.children[i]);
+    }
+    res += `</${obj.name}>`;
+  } else {
+    res += "/>";
+  }
+
+  return res;
+}
+
+function generateCodeFromObject(obj) {
+  return generateJSX(obj);
+}
+
+module.exports = generateCodeFromObject;
 
 // ReactDOM.render(<App />, document.getElementById("root"));
